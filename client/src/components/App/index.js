1b58c71bac9da0ad9eb2c8420efe3d3e402d4ac5
twliew <twliew@uwaterloo.ca>
2024-02-11 23:14:26 -0500

    app directory for app pages

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import Review from './Review';
+import Home from './Home';
+import Profile from './Profile';
+import People from './People';
+import Matches from './Matches';
 
 function App() {
-  return <Review />;
+  return <Home />;
+  return <Profile />;
+  return <People />;
+  return <Matches />;
+
 }
 
 export default App;
