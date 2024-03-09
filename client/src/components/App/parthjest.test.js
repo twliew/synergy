02031a8df577a22e1e245bb6d1a089527187dc5f
Parth Desai <38592942+Parthdsi@users.noreply.github.com>
2024-03-09 14:58:59 -0500

    Update parthjest.test.js

--- client/src/components/App/parthjest.test.js
+++ client/src/components/App/parthjest.test.js
@@ -1,15 +1,42 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import Login from './Login';
+import Profile from '../Profile';
 
-describe('Login Component', () => {
-    it('calls onLogin prop function when form is submitted with valid data', async () => {
-        const mockOnLogin = jest.fn();
-        render(<Login onLogin={mockOnLogin} />);
-        fireEvent.change(screen.getByLabelText('Username'), { target: { value: 'testuser' } });
-        fireEvent.change(screen.getByLabelText('Password'), { target: { value: 'testpassword' } });
-        fireEvent.click(screen.getByRole('button', { name: 'Login' }));
-        await screen.findByText('Login');
-        expect(mockOnLogin).toHaveBeenCalledTimes(1);
+describe('Login component', () => {
+    test('renders login button correctly', () => {
+        render(<Login />);
+        expect(screen.getByRole('button', { name: 'Login' })).toBeInTheDocument();
     });
+});
+
+describe('Profile component', () => {
+    test('renders Interests and Hobbies on page correctly', () => {
+        render(<Profile />);
+        expect(screen.getByText('Interests/Hobbies')).toBeInTheDocument();
+    });
+
+    test('renders add social media button correctly', () => {
+        render(<Profile />);
+        expect(screen.getByRole('button', { name: 'Add Social Media' })).toBeInTheDocument();
+    });
+
+    test('renders cancel button when add social media button is clicked', async () => {
+        render(<Profile />);
+        const addSocialMediaButton = screen.getByRole('button', { name: 'Add Social Media' });
+        fireEvent.click(addSocialMediaButton);
+        await waitFor(() => {
+            expect(screen.getByRole('button', { name: 'Cancel' })).toBeInTheDocument();
+        });   
+    });
+
+    test('renders add hobby button when edit interests button is clicked', async () => {
+        render(<Profile />);
+        const editInterestsButton = screen.getByRole('button', { name: 'Edit Interests' });
+        fireEvent.click(editInterestsButton);
+        await waitFor(() => {
+            expect(screen.getByRole('button', { name: 'Add Hobby' })).toBeInTheDocument();
+        });
+      });
 });
