9fff92b12878b70f1001719b384aded0fdc83500
twliew <twliew@uwaterloo.ca>
2024-03-15 10:57:06 -0400

    e2e

--- client/cypress/e2e/smoke.cy.js
+++ client/cypress/e2e/smoke.cy.js
@@ -1,6 +1,19 @@
-describe('empty spec', () => {
+describe('render login', () => {
     it('can view the home page', () => {
-    cy.visit('/Login');
-    cy.contains('Login');
+        cy.visit('/Login');
+        cy.contains('Login');
     });
+
+    it('login', () => {
+        cy.visit('/Login');
+
+        cy.get('input[name="username"]').type('Parthdsi');
+        cy.get('input[name="password"]').type('Parthdesai1@');
+
+        cy.get('form').submit();
+
+        //cy.url().should('include', '/dashboard');
+
+        cy.contains('Welcome,').should('be.visible');
     });
+});
