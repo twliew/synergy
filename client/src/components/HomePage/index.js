148166eba83bb3cc22da77315bc969969fdaf3d7
Parthdsi <parthd830@gmail.com>
2024-04-01 12:09:45 -0400

    commititng

--- client/src/components/HomePage/index.js
+++ client/src/components/HomePage/index.js
@@ -1,30 +1,38 @@
 import * as React from 'react';
 import Typography from "@mui/material/Typography";
-import { createTheme, ThemeProvider} from '@mui/material/styles'
-import Grid from "@mui/material/Grid";
 import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Container from '@mui/material/Container';
 import { useNavigate } from 'react-router-dom';
-
-const theme = createTheme({
-    palette: {
-        mode: 'light',
-    },
-});
+import ViewLikes from '../People/ViewLikes';
 
 const HomePage = () => {
-
     const navigate = useNavigate();
+    const [showViewLikes, setShowViewLikes] = React.useState(false);
+
+    const handleViewLikes = () => {
+        setShowViewLikes(true); // Set showViewLikes to true when the "View Likes" button is clicked
+    };
+
+    const handleHideLikes = () => {
+        setShowViewLikes(false); // Set showViewLikes to false when the "Hide Likes" button is clicked
+    };
 
     return (
-        <ThemeProvider theme={theme}>
-            {/*overhead bar*/}
-      <Typography>home</Typography>
-    </ThemeProvider>
-  );
+        <div>
+            <Typography variant="h2" gutterBottom>Welcome to Synergy!</Typography>
+            <Typography variant="body1">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar dui non est maximus, eu ultricies mi sodales.
+                Integer nec magna nec dui scelerisque finibus. Sed nec ullamcorper libero. Proin fermentum fermentum mauris,
+                sed viverra ligula commodo sed. Duis sit amet urna eget leo fringilla luctus. Nulla facilisi. Donec at orci eu
+                odio finibus tristique vel sed justo. Ut rhoncus lectus nisi, at aliquet justo aliquam nec. Nam at turpis dolor.
+            </Typography>
+            {/* "View Likes" button */}
+            <Button onClick={handleViewLikes} variant="contained" color="primary">View Likes</Button>
+            {/* "Hide Likes" button */}
+            {showViewLikes && <Button onClick={handleHideLikes} variant="contained" color="secondary">Hide Likes</Button>}
+            {/* Render the ViewLikes component only if showViewLikes is true */}
+            {showViewLikes && <ViewLikes />}
+        </div>
+    );
 }
 
 export default HomePage;
